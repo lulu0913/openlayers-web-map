bcff0a307d67105c0ad49f61cece0ab772d76189
lulu0913 <1029390056@qq.com>
2020-07-28 14:39:51 +0800

    切换接触地图

--- main.js
+++ main.js
@@ -4,18 +4,18 @@ function init(){
     const map = new ol.Map({
         view: new ol.View({
             center: [0, 0],
-            zoom: 2,
+            zoom: 4,
             maxZoom: 10,
-            minZoom: 4,
+            minZoom: 2,
             // rotation: 0.5
         }),
         target: "js-map"
     })
 
     // Basemaps Layers
-    const openStreetMaoStandard = new ol.layer.Tile({
+    const openStreetManStandard = new ol.layer.Tile({
         source: new ol.source.OSM(),
-        visible: false,
+        visible: true,
         title: "OSMStandard"
     })
 
@@ -32,15 +32,27 @@ function init(){
             url: "",
             attributions: ""
         }),
-        visible: true,
+        visible: false,
         title: "StanmenTerrain"
     })
 
     // layer Group
     const baseLayerGroup = new ol.layer.Group({
         layers:[
-            openStreetMaoStandard, openStreetMapHumanitarian, stamenTerrain
+            openStreetManStandard, openStreetMapHumanitarian, stamenTerrain
         ]
     })
     map.addLayer(baseLayerGroup);
+
+    //layer switch logic for basemaps
+    const BaseLayerElements = document.querySelectorAll(".sidebar > input[type = radio]");
+    for(let BaseLayerElement of BaseLayerElements){
+        BaseLayerElement.addEventListener("change", function(){
+            let BaseLayerElementValue = this.value;
+            baseLayerGroup.getLayers().forEach(function(element, index, array){
+                let baseLayerTitle = element.get("title");
+                element.setVisible(BaseLayerElementValue === baseLayerTitle);
+            });
+        })
+    }
 }
